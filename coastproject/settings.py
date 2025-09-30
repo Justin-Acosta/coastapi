e3c8bc08d3524bd64745b2b33e5088878fee649a
Justin-Acosta <jacostaprof@gmail.com>
2025-09-30 11:37:19 -0500

    updating settings

--- coastproject/settings.py
+++ coastproject/settings.py
@@ -134,5 +134,5 @@ MEDIA_URL = '/media/'
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False")
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", '').split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False")
